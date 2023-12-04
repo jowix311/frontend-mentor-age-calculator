3d11e50bf9fd01b37c39a1a5fde5a735e3cd8803
jowix <jowix311@gm,ail.com>
2023-12-04 23:09:03 +0800

    Form - added react hook form, added initial validation logic and style, has todo items

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,86 @@
 import "./App.css";
 import InputField from "./components/input-field/InputField.tsx";
 import DateDetail from "./components/date-detail/DateDetail.tsx";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+export type Inputs = {
+  day: string;
+  month?: string;
+  year?: string;
+};
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className="bg-offWhite min-h-screen pt-32">
       <div
         className="w-[325px] bg-white m-auto pt-8 pr-5 pl-5 pb-10 rounded-xl rounded-br-xxl
         "
       >
-        <div className="flex gap-2  mb-6">
-          <InputField
-            label="Day"
-            id="day"
-            type="text"
-            name="day"
-            value=""
-            placeholder="DD"
-            maxLength={2}
-          />
-          <InputField
-            label="Month"
-            id="day"
-            type="text"
-            name="day"
-            value=""
-            placeholder="MM"
-            maxLength={2}
-          />
-          <InputField
-            label="Year"
-            id="day"
-            type="text"
-            name="day"
-            value=""
-            placeholder="YYYY"
-            maxLength={4}
-          />
-        </div>
-
-        <div className="relative flex justify-center items-center  mb-6">
-          <div
-            role="button"
-            className="relative cursor-pointer z-10 bg-purple hover:bg-black w-16 h-16 rounded-full flex justify-center items-center"
-          >
-            <img
-              className="w-auto h-8 "
-              alt="age caluclator divider image"
-              src="../src/assets/icon-arrow.svg"
+        <form onSubmit={handleSubmit(onSubmit)} noValidate={true}>
+          <div className="flex gap-2  mb-6">
+            <InputField
+              label="Day"
+              errors={errors}
+              id="day"
+              type="text"
+              name="day"
+              placeholder="DD"
+              maxLength={2}
+              register={{
+                ...register("day", {
+                  required: {
+                    value: true,
+                    message: "This field is required!",
+                  },
+                  pattern: {
+                    value: /^[0-9]+$/,
+                    message: "Please enter a number",
+                  },
+                }),
+              }}
             />
+            {/*/!*<InputField*!/*/}
+            {/*  label="Month"*/}
+            {/*  id="day"*/}
+            {/*  type="text"*/}
+            {/*  name="day"*/}
+            {/*  value=""*/}
+            {/*  placeholder="MM"*/}
+            {/*  maxLength={2}*/}
+            {/*/>*/}
+            {/*<InputField*/}
+            {/*  label="Year"*/}
+            {/*  id="day"*/}
+            {/*  type="text"*/}
+            {/*  name="day"*/}
+            {/*  value=""*/}
+            {/*  placeholder="YYYY"*/}
+            {/*  maxLength={4}*/}
+            {/*/>*/}
           </div>
-          <div className="absolute right-0 left-0 border-b-2 border-lightGray border-solid"></div>
-        </div>
 
+          <div className="relative flex justify-center items-center  mb-6">
+            <button
+              role="button"
+              className="relative cursor-pointer z-10 bg-purple hover:bg-black w-16 h-16 rounded-full flex justify-center items-center"
+            >
+              <img
+                className="w-auto h-8 "
+                alt="age caluclator divider image"
+                src="../src/assets/icon-arrow.svg"
+              />
+            </button>
+            <div className="absolute right-0 left-0 border-b-2 border-lightGray border-solid"></div>
+          </div>
+        </form>
         <div className="flex flex-col direction gap-2">
           <DateDetail day={38} label="years" />
           <DateDetail day={3} label="months" />
--- src/components/input-field/InputField.tsx
+++ src/components/input-field/InputField.tsx
@@ -1,27 +1,53 @@
+import { FieldErrors } from "react-hook-form";
+import { Inputs } from "../../App.tsx";
+
 interface InputFieldProps {
   label: string;
   id: string;
   type: string;
   name: string;
   placeholder: string;
-  value: string;
+  value?: string;
   maxLength?: number;
-  onChange?: () => void;
+  register: object; //@todo add proper typing
+  errors?: FieldErrors<Inputs>; //@todo add proper typing - reason Inputs is not so dynamic
+  options?: object;
 }
 
-const InputField = ({ label, ...otherProps }: InputFieldProps) => {
+const InputField = ({
+  label,
+  register,
+  errors,
+  ...otherProps
+}: InputFieldProps) => {
+  const targetKey = otherProps.id as keyof FieldErrors<Inputs>;
+
   return (
     <div>
       <label
-        htmlFor="day"
-        className="block font-poppins uppercase text-smokeyGray text-xs mb-2 tracking-wider"
+        htmlFor={otherProps.id}
+        className={`block font-poppins uppercase text-smokeyGray text-xs mb-2 tracking-wider ${
+          errors![targetKey] && "text-lightRed"
+        }`}
       >
         {label}
       </label>
       <input
+        {...register}
         {...otherProps}
-        className="h-12 border-solid border border-lightGray rounded-lg pl-3 font-poppins text-offBlack focus:outline-none focus:border-purple active:border-purple hover:border-purple w-full caret-purple"
+        className={`h-12 border-solid border border-lightGray
+        rounded-lg pl-3 font-poppins text-offBlack
+        focus:outline-none focus:border-purple active:border-purple
+        hover:border-purple w-full caret-purple invalid ${
+          errors![targetKey] &&
+          "border-lightRed focus:border-lightRed active:border-lightRed hover:border-lightRed caret-lightRed"
+        }`}
       />
+      {errors!.day && (
+        <span className="text-lightRed text-xs font-poppins italic">
+          {errors![targetKey]!.message}
+        </span>
+      )}
     </div>
   );
 };
