1a676456eb7200d64ca9e052903a94e03f0f9a60
jowix <jowix311@gm,ail.com>
2023-12-20 12:53:27 +0800

    fix bug - age breakdown does not re-render on next submit

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -8,18 +8,22 @@ const Counter = ({ maxCount }: CounterProps) => {
   const [currentCount, setCurrentCount] = useState(0);
 
   useEffect(() => {
+    setCurrentCount(0); // everytime component re-renders (due to maxCount updating) we set back currentCount to 0
+  }, [maxCount]);
+
+  useEffect(() => {
     let intervalId: number | undefined = undefined;
 
     const updateCount = () => {
       if (currentCount < maxCount) {
-        setCurrentCount((prevCurrentCount) => prevCurrentCount + 1);
+        setCurrentCount((prevState) => prevState + 1);
       } else {
         clearInterval(intervalId);
       }
     };
     intervalId = setInterval(updateCount, 100);
 
-    return () => clearInterval(intervalId);
+    return () => clearInterval(intervalId); //on component unmount clear Interval
   }, [currentCount, maxCount]);
 
   return <>{currentCount}</>;
